fdd4c93572139b4dcf4ee12f030d0b03eb508bdd
Assasz <pawel.assasz678@gmail.com>
2018-05-11 21:22:58 +0200

    Add todo edit feature, prepare all modals

--- api/src/main/java/api/TodoController.java
+++ api/src/main/java/api/TodoController.java
@@ -2,7 +2,13 @@ package api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Date;
 
 import api.Todo;
 import api.TodoRepository;
@@ -22,5 +28,16 @@ public class TodoController {
 	@RequestMapping(value = "/todo/{id}", method = RequestMethod.GET)
 	public Todo get(@PathVariable Integer id) {
 		return this.todoRepository.getOne(id);
+	}
+
+	@RequestMapping(value = "/todo/{id}", method = RequestMethod.PUT)
+	public Todo edit(@PathVariable Integer id, @RequestBody Todo body) {
+		Todo todo = this.todoRepository.getOne(id);
+
+        todo.setTitle(body.getTitle());
+		todo.setContent(body.getContent());
+		todo.setDueDate(body.getDueDate());
+
+		return this.todoRepository.save(todo);
 	}
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,6 +9,54 @@ const Home = {
             var now = new Date().toString();
 
             return moment(String(value)).diff(moment(now), type);
+        },
+        toggleModal: function(target, event, todoId = null) {
+            event.preventDefault();
+            $('#' + target).modal();
+
+            if(todoId !== null){
+                $('#' + target).find('form').attr('data-id', todoId);
+
+                if(target === 'edit_modal'){
+                    $.ajax({
+                        method: 'get',
+                        url: "http://localhost:8080/todo/" + todoId
+                    }).then(function(data, status, jqxhr) {
+                        $('#edit_title').val(data.title);
+                        $('#edit_content').val(data.content);
+                        $('#edit_due_date').val(moment(data.dueDate).format('YYYY-MM-DDTHH:mm'));
+                    });
+                }
+            }
+        },
+        addTodo: function(event) {
+
+        },
+        editTodo: function(event) {
+            var todoId = $('#edit_form').data('id'),
+                data = {
+                    title: $('#edit_title').val(),
+                    content: $('#edit_content').val(),
+                    dueDate: $('#edit_due_date').val(),
+                };
+
+            $.ajax({
+                method: 'put',
+                url: "http://localhost:8080/todo/" + todoId,
+                data: JSON.stringify(data),
+                contentType: "application/json"
+            }).then(function(data, status, jqxhr) {
+                var dueDate = moment(String(data.dueDate)).endOf('minutes').fromNow() + ' - ' + moment(String(data.dueDate)).format('MMMM Do YYYY, hh:mm');
+
+                $('#' + todoId + ' #todo_title').html(data.title);
+                $('#' + todoId + ' #todo_due_date').html(dueDate);
+                $('#' + todoId + ' #todo_content').html(data.content);
+
+                $('#edit_modal').modal('hide');
+            });    
+        },
+        deleteTodo: function(event) {
+            
         }
     },
     mounted() {
@@ -29,30 +77,117 @@ const Home = {
         }
     },
     template: `
-    <div id="home">
-        <h1 class="mt-4">My ToDo's</h1>
-        <div class="row mt-4">
-            <div class="col-lg-6">
-                <label for="search" class="offscreen">Search todos</label>
-                <input type="text" class="form-control" id="search" placeholder="Search todos">
-            </div>
-            <div class="col-lg-6 mt-4 mt-lg-0">
-                <a href="add" class="btn btn-primary" role="button">Add todo</a>
-            </div>
-        </div>
-        <div class="card-columns mt-4">
-            <div v-bind:class="[diffDates(todo.dueDate) > 0 ? 'card border-dark mb-3' : 'card border-warning mb-3']" v-for="todo in todos">
-                <div class="card-header">
-                    <h2 class="card-title mb-0">{{ todo.title }}</h2>
-                    <p v-bind:class="[diffDates(todo.dueDate, 'day') <= 1 && diffDates(todo.dueDate) > 0 ? 'text-danger mt-2' : 'text-muted mt-2']">{{ todo.dueDate|formatDate }}</p>
+        <div id="home">
+            <h1 class="mt-4">My ToDo's</h1>
+            <div class="row mt-4">
+                <div class="col-lg-6">
+                    <label for="search" class="offscreen">Search todos</label>
+                    <input type="text" class="form-control" id="search" name="search" placeholder="Search todos">
                 </div>
-                <div class="card-body">
-                    <p class="card-text mb-3">{{ todo.content }}</p>
-                    <a href="#" class="card-link">Edit</a>
-                    <a href="#" class="card-link">Delete</a>
+                <div class="col-lg-6 mt-4 mt-lg-0">
+                    <button type="button" class="btn btn-primary" v-on:click="toggleModal('create_modal', $event)">Add todo</a>
                 </div>
             </div>
+
+            <div id="todos" class="card-columns mt-4">
+                <div v-bind:id="todo.id" v-bind:class="[diffDates(todo.dueDate) > 0 ? 'card border-dark mb-3' : 'card border-warning mb-3']" v-for="todo in todos">
+                    <div class="card-header">
+                        <h2 id="todo_title" class="card-title mb-0">{{ todo.title }}</h2>
+                        <p id="todo_due_date" v-bind:class="[diffDates(todo.dueDate, 'day') <= 1 && diffDates(todo.dueDate) > 0 ? 'text-danger mt-2' : 'text-muted mt-2']">{{ todo.dueDate|formatDate }}</p>
+                    </div>
+                    <div class="card-body">
+                        <p id="todo_content" class="card-text mb-3">{{ todo.content }}</p>
+                        <a href="#" class="card-link" v-on:click="toggleModal('edit_modal', $event, todo.id)">Edit</a>
+                        <a href="#" class="card-link" v-on:click="toggleModal('delete_modal', $event, todo.id)">Delete</a>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="create_modal" class="modal fade">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Add todo</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="create_form">
+                                    <div class="form-group">
+                                        <label for="title">Title</label>
+                                        <input type="text" class="form-control" id="title" name="title">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="content">Content</label>
+                                        <textarea rows="3" class="form-control" id="content" name="content"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="due_date">Due date</label>
+                                        <input type="datetime-local" class="form-control" id="due_date" name="dueDate">
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary" v-on:click="addTodo">Save changes</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            <div id="edit_modal" class="modal fade">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Edit todo</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form id="edit_form">
+                                <div class="form-group">
+                                    <label for="edit_title">Title</label>
+                                    <input type="text" class="form-control" id="edit_title" name="title">
+                                </div>
+                                <div class="form-group">
+                                    <label for="edit_content">Content</label>
+                                    <textarea rows="3" class="form-control" id="edit_content" name="content"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="edit_due_date">Due date</label>
+                                    <input type="datetime-local" class="form-control" id="edit_due_date" name="dueDate">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" v-on:click="editTodo">Save changes</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="delete_modal" class="modal fade">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Delete todo</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Are you sure you want to delete this todo?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" v-on:click="deleteTodo">Delete</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     `
 };
